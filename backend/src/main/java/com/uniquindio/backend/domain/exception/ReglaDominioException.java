9db6aeb8ecba050def04ab18de7a689dc6d1c2b8
Juan David Tapiero <juand.tapierom@uqvirtual.edu.co>
2026-09-12 12:13:15 -0500

    Add custom domain exception: ReglaDominioException

---
+++ backend/src/main/java/com/uniquindio/backend/domain/exception/ReglaDominioException.java
@@ -0,0 +1,7 @@
+package com.uniquindio.backend.domain.exception;
+
+public class ReglaDominioException extends RuntimeException {
+    public ReglaDominioException(String message) {
+        super(message);
+    }
+}
